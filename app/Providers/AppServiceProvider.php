7f182033208973bd2a194c2db86246599ad7f961
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-02-27 13:14:03 -0600

    Se realizaron las primeras configuraciones para que laravel funcione con react

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Configuramos manual mente la longitud para 
+         * versiones menores de mysql
+         */
+        Schema::defaultStringLength(191);
     }
 }
